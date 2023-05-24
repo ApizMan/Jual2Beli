05e0b60ffe72d6bbd54d277ea06214cf018854a9
Izzati <n.izzahtini@gmail.com>
2023-05-24 15:21:28 +0800

    cart

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -62,10 +62,7 @@
                     <p class="summary-info total-info "><span class="title">Total</span><b class="index">{{ Cart::total() }}</b></p>
                 </div>
                 <div class="checkout-info">
-                    <label class="checkbox-field">
-                        <input class="frm-input " name="have-code" id="have-code" value="" type="checkbox"><span>I have
-                            promo code</span>
-                    </label>
+                    
                     <a class="btn btn-checkout" href="/checkout">Check out</a>
                     <a class="link-to-shop" href="{{ url('/shop') }}">Continue Shopping<i class="fa fa-arrow-circle-right"
                             aria-hidden="true"></i></a>
